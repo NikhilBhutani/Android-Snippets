7a63e2395e3f42829c797b2e873a0aea5156c33e
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-06-10 02:49:54 +0530

    Firebase added some code

--- Firebase/app/src/main/java/github/nikhilbhutani/firebase/MainActivity.java
+++ Firebase/app/src/main/java/github/nikhilbhutani/firebase/MainActivity.java
@@ -1,6 +1,5 @@
 package github.nikhilbhutani.firebase;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,54 +11,29 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity {
 
-    SignInButton signInButton;
-    TextView statusTextView;
 
-    GoogleApiClient googleApiClient;
+
+
     //you get this URL when you create your firebase database,
     //This is actually the connection String.
-    static final String firebaseURL = "https:<YOUR_FIREBASE_URL>.firebaseio.com/";
-    private static final int RC_SIGN_IN = 9001;
+    static final String firebaseURL = "https://ADD YOUR APP URL.firebaseio.com/";
+
     private EditText editTextName;
     private TextView textViewUsers;
     private Button buttonSave;
-    @Override
+
+ SignInButton signInButton;
+    TextView statusTextView;
+
+    GoogleApiClient googleApiClient; 
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        statusTextView = (TextView)findViewById(R.id.statustextview);
-        GoogleSignInOptions gso = new GoogleSignInOptions
-                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-
-
-       googleApiClient = new GoogleApiClient
-               .Builder(this)
-               .enableAutoManage(this /* FragmentActivity */, this /*onConnectionFailedListener*/)
-               .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-               .build();
-
-        signInButton = (SignInButton)findViewById(R.id.signIn_button);
-
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
 
         //Setting the context for Firebase
         Firebase.setAndroidContext(this);
@@ -120,41 +94,4 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             }
         });
     }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-        System.out.print("Connection Failed");
-    }
-
-
-    private void signIn()
-    {
-        Intent signIn_intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(signIn_intent, RC_SIGN_IN);
-        System.out.println("signIn method");
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //Result returned from launching the intent from GoogleSignInApi.getSignInIntent(..);
-        if(requestCode == RC_SIGN_IN)
-        {
-            System.out.println("startActivity method");
-            GoogleSignInResult result =
-                    Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            GoogleSignInAccount acct = result.getSignInAccount();
-            statusTextView.setText("Hello, " + acct.getDisplayName());
-        } else {
-        }
-    }
-
 }
