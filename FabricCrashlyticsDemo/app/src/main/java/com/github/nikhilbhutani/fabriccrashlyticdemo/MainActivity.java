ca1fca9bac8191a076a8b0fd075997936f2cac35
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-10-16 08:10:53 +0530

    Implementing Crashlytics

---
+++ FabricCrashlyticsDemo/app/src/main/java/com/github/nikhilbhutani/fabriccrashlyticdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.github.nikhilbhutani.fabriccrashlyticdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+    }
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("This is a crash");
+    }
+
+}
