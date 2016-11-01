8d6d56727107f0278a72454b12dd70f18fc662d6
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-11-01 13:07:34 +0530

    Adding new Bottom Navigation view in Android

---
+++ BottomNavigationViewSample/app/src/main/java/com/github/nikhilbhutani/bottomnavigationviewsample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.github.nikhilbhutani.bottomnavigationviewsample;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
